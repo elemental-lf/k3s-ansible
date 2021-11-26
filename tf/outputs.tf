012e11a76b92c558a5f124fcf7d810f65018f2e0
Lars Fenneberg <lf@elemental.net>
2021-11-26 17:30:33 +0100

    Add additional outputs to TF module

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,3 +1,11 @@
 output "kubeconfig_path" {
   value = local.k3s_config_yaml["kubeconfig_path"]
 }
+
+output "hosts_ini_path" {
+  value = local_file.hosts_ini.filename
+}
+
+output "k3s_config_yaml_path" {
+  value = local_file.k3s_config_yaml.filename
+}
